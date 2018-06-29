266695e28cc88f2d4e855731b271dc60c44128f4
Sultan Imbayev <sultan.imbayev@gmail.com>
2018-06-29 11:48:05 +0600

    example modified

---
+++ TestProj.BLL/Forms/Export/Handlers/MyFileHandler.cs
@@ -0,0 +1,27 @@
+﻿using IEIT.Reports.WebFramework.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProj.BLL.Forms.Export.Repositories;
+
+namespace TestProj.BLL.Forms.Export.Handlers
+{
+    public class MyFileHandler : IHandler
+    {
+        MyFileRepo repo;
+        public void InitializeRepo(object repository)
+        {
+            repo = (MyFileRepo)repository;
+        }
+    
+        public void GenerateFiles(string inDir)
+        {
+            var filePath = Path.Combine(inDir, "file.txt");
+            File.WriteAllText(filePath, repo.Content);
+        }
+
+    }
+}
---
+++ TestProj.BLL/Forms/Export/Repositories/MyFileRepo.cs
@@ -0,0 +1,24 @@
+﻿using IEIT.Reports.WebFramework.Core.Attributes;
+using IEIT.Reports.WebFramework.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProj.BLL.Forms.Export.Repositories
+{
+    [RepositoryFor("MyFile")]
+    [HasHandler("MyFileHandler")]
+    public class MyFileRepo : IRepository
+    {
+        public string Content { get; set; }
+
+        public void Init(NameValueCollection queryParams)
+        {
+            var name = queryParams["name"] ?? "всем";
+            Content = $"Привет {name}!";
+        }
+    }
+}
